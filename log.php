a5b5c660ae4eb110777249dbe8820eecdcb8112d
Man-gel <night_captain@hotmail.com>
2015-03-10 17:21:54 -0600

    Inicio de la practica

---
+++ log.php
@@ -0,0 +1,30 @@
+<html>
+   <body>
+	<div>
+  	   <form action="calcular.php" method="POST">
+		<table>
+		   <tr>
+		    <th>Cantidad</th>
+		    <th>Producto</th>
+		    <th>$ Unitario</th>
+		   </tr>
+		   <tr>
+		    <td><input type="text" size="10" maxlength="5" id="cant1" value=""/></td>
+		    <td><input type="text" size="20" maxlength="20" id="prod1" value=""/></td>
+		    <td><input type="text" size="10" maxlength="6" id="punit1" value=""/></td>
+		   </tr>
+		   <tr>
+		    <td><input type="text" size="10" maxlength="5" id="cant2" value=""/></td>
+                    <td><input type="text" size="20" maxlength="20" id="prod2" value=""/></td>
+                    <td><input type="text" size="10" maxlength="6" id="punit2" value=""/></td>
+		   </tr>
+		   <tr>
+		    <td><input type="text" size="10" maxlength="5" id="cant3" value=""/></td>
+                    <td><input type="text" size="20" maxlength="20" id="prod3" value=""/></td>
+                    <td><input type="text" size="10" maxlength="6" id="punit3" value=""/></td>
+		   </tr>		
+		</table>
+	   </form>
+	</div>
+   </body>
+</html>
