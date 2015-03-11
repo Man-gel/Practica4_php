75fb2236e7602b50292de86895cf37c929ed6bf9
Man-gel <night_captain@hotmail.com>
2015-03-10 18:40:58 -0600

    Se terminaron los dos archivos

--- cobro.php
+++ cobro.php
@@ -36,16 +36,54 @@
       $subt2 = $prod2['cant'] * $prod2['punit'];
       $subt3 = $prod3['cant'] * $prod3['punit'];
       $subtotal = $subt1 + $subt2 + $subt3;
+      $IVA = ($subt1 + $subt2 + $subt3)*0.16;
       $total = ($subt1 + $subt2 + $subt3)*1.16;
  //     var_dump(subt1);
     ?>
   <div>
-  <?php
- 
-    echo "<h1>$subt1</h1>";
-    echo "<h1>$subt2</h1>"; 
-    
-  ?>
+    <table>
+	   <tr>
+	    <th>Producto</th>
+	    <th>Cantidad</th>
+	    <th>$ Unitario</th>
+	   </tr>
+	   <tr>
+	    <td><?php echo $prod1['prod']?></td>
+	    <td><?php echo $prod1['cant']?></td>
+	    <td><?php echo $prod1['punit']?></td>
+	   </tr>
+	   <tr>
+	    <td><?php echo $prod2['prod']?></td>
+	    <td><?php echo $prod2['cant']?></td>
+	    <td><?php echo $prod2['punit']?></td>
+	   </tr>
+	   <tr>
+	    <td><?php echo $prod3['prod']?></td>
+	    <td><?php echo $prod3['cant']?></td>
+	    <td><?php echo $prod3['punit']?></td>
+	   </tr>		
+	</table>
+   </div>
+   <br/>
+   <br/>
+   <div>
+    <table>
+	   <tr>
+	    <td><?php echo "SUBTotal"?></td>
+	    <td><?php echo " 	$"?></td>
+	    <td><?php echo $subtotal?></td>
+	   </tr>
+	   <tr>
+	    <td><?php echo "IVA "?></td>
+	    <td><?php echo "	$"?></td>
+	    <td><?php echo $IVA?></td>
+	   </tr>
+	   <tr>
+	    <td><?php echo "TOTAL $"?></td>
+	    <td><?php echo ""?></td>
+	    <td><?php echo $total?></td>
+	   </tr>		
+	</table>
    </div>
  </body>
 </html>
